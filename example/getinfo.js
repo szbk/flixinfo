572fcb8623d98c902a1f55fe139e4280c6b660ea
Wise Colt <wisecolt-macbook@Wise-MacBook-Pro.local>
2019-01-01 01:34:53 +0300

    error message update

--- example/getinfo.js
+++ example/getinfo.js
@@ -4,7 +4,7 @@ const getflix = new GetFlix({ tmdbApiKey: 'f29e56ff85f361ff01b5c5403a343021' });
 try {
     getflix.getInfo('70143836', (res) => {
         if (res.error) {
-            console.log(err);
+            console.log(res.errorMsg);
         }
         else {
             console.log(res);
