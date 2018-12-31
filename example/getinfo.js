604a237d0866bbcfc170c24ac6f0f3ac59185e73
Wise Colt <wisecolt-macbook@Wise-MacBook-Pro.local>
2019-01-01 01:22:48 +0300

    error system change

--- example/getinfo.js
+++ example/getinfo.js
@@ -1,13 +1,17 @@
 const GetFlix = require('../index');
+const getflix = new GetFlix({ tmdbApiKey: 'f29e56ff85f361ff01b5c5403a343021' });
 
-const getflix = new GetFlix({tmdbApiKey : 'f29e56ff85f361ff01b5c5403a343021'});
-
-getflix.getInfo('70143836', (err, res)=>{
-    if(err){
-        console.log(err);
-    }
-    else{
-        console.log(res);
-    }
-})
+try {
+    getflix.getInfo('70143836', (res) => {
+        if (res.error) {
+            console.log(err);
+        }
+        else {
+            console.log(res);
+        }
+    })
+}
+catch (error) {
+    console.log(error.message)
+}
 
