382e975789aac2190cccb1e2bb68f3b4acf0e45d
Wise Colt <wisecolt-macmini@Wise-Mac-mini.local>
2020-03-28 03:02:51 +0300

    update example

--- example/getinfo.js
+++ example/getinfo.js
@@ -3,14 +3,11 @@ const GetFlix = require('../index');
 const getflix = new GetFlix('d9d6007d1bcf12043db5a085ae3e5bbb');
 const getInfo = async () => {
     try {
-        const info = await getflix.getInfo('802329261');
-        if(info == null) {
-            console.log('null');
-            
-        }
+        const info = await getflix.getInfo('80244780');
+        console.log(info);
 
     } catch (error) {
-        // console.log(error);
+        console.log(error);
     }
 }
 
