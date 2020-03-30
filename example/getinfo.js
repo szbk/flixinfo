83f89695c9e96b5194c1a2d1b9558c7aac12edc9
Wise Colt <wisecolt-macmini@Wise-Mac-mini.local>
2020-03-31 02:05:55 +0300

    bu fixes

--- example/getinfo.js
+++ example/getinfo.js
@@ -3,7 +3,8 @@ const GetFlix = require('../index');
 const getflix = new GetFlix('d9d6007d1bcf12043db5a085ae3e5bbb');
 const getInfo = async () => {
     try {
-        const info = await getflix.get(80209184, 'tr-TR');
+        const info = await getflix.get(80244780, 'tr-TR');
+        // info.credits.cast.forEach((f) => console.log(f.profile_path));
         console.log(info);
 
     } catch (error) {
