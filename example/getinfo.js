f80585173f3e180f7098fb94b797f0aff054da1f
Wise Colt <wisecolt-macmini@Wise-Mac-mini.local>
2020-03-31 21:44:15 +0300

    example api key hidden

--- example/getinfo.js
+++ example/getinfo.js
@@ -1,10 +1,9 @@
 const GetFlix = require('../index');
 
-const getflix = new GetFlix('d9d6007d1bcf12043db5a085ae3e5bbb');
+const getflix = new GetFlix('YOUR API KEY');
 const getInfo = async () => {
     try {
-        const info = await getflix.get(80244780, 'tr-TR');
-        // info.credits.cast.forEach((f) => console.log(f.profile_path));
+        const info = await getflix.get(80241239, 'en-EN');
         console.log(info);
 
     } catch (error) {
