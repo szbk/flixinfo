3ef0cf0e8571ac9d7658c5f73bb0e6791a0f63af
sbilgetay <wisecolt@gmail.com>
2020-04-27 19:44:56 +0300

    add netflix info method

--- example/getinfo.js
+++ example/getinfo.js
@@ -1,9 +1,10 @@
 const GetFlix = require('../index');
 
-const getflix = new GetFlix('YOUR API KEY');
+const getflix = new GetFlix('d9d6007d1bcf12043db5a085ae3e5bbb');
 const getInfo = async () => {
     try {
-        const info = await getflix.get(80241239, 'en-EN');
+        const info = await getflix.getNetflixInfo(81159258);
+        // const info = await getflix.get(80241239, 'en-EN', 'tr');
         console.log(info);
 
     } catch (error) {
