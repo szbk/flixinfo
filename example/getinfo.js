fd58d435683ad36241000766010491c7a4255da1
Wise Colt <wisecolt-macmini@Wise-Mac-mini.local>
2020-04-29 13:36:31 +0300

    big change in the results returned by the api

--- example/getinfo.js
+++ example/getinfo.js
@@ -1,12 +1,10 @@
 const GetFlix = require('../index');
-
 const getflix = new GetFlix('d9d6007d1bcf12043db5a085ae3e5bbb');
-const getInfo = async () => {
-    try {
-        const info = await getflix.getNetflixInfo(81159258);
-        // const info = await getflix.get(80241239, 'en-EN', 'tr');
-        console.log(info);
 
+getInfo = async () => {
+    try {
+        const info = await getflix.getNetflixInfo(81082225, 'en-EN', 'en');
+        console.log(info);
     } catch (error) {
         console.log(error);
     }
